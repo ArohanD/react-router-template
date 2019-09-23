c9fc65b272d4a78fb875bd302f81a65795d142c4
Arohan Dutt <arohandutt@gmail.com>
2019-09-23 11:07:38 -0500

    Save router-dom

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('../dist'));
+app.use(express.static('dist'));
 
 app.listen(port, () => console.log(`listening from port: ${port}`));
