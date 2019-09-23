1f62ca74b023199af3b66d2c71bfe3e804ac6f54
Arohan Dutt <arohandutt@gmail.com>
2019-09-23 12:04:01 -0500

    Add router refresh handle

--- server/server.js
+++ server/server.js
@@ -4,4 +4,14 @@ const port = 3000;
 
 app.use(express.static('dist'));
 
+
+//Add new routes above
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.listen(port, () => console.log(`listening from port: ${port}`));
